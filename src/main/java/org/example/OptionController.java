ce5e5c121214b9c93d049ef9848a6b9b8401f82b
Piotr Partyka <arenx92@gmail.com>
2023-01-14 19:51:28 +0100

    Implement Save Log option

---
+++ src/main/java/org/example/OptionController.java
@@ -0,0 +1,43 @@
+package org.example;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class OptionController {
+
+    private BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+    private UsersHolder usersHolder = new UsersHolder();
+    private LogHolder logHolder = new LogHolder();
+
+    public void getStarted() {
+        while (true) {
+            String option = "";
+            System.out.println("Do you want to review saved Logs or create a new one? review/save");
+
+            try {
+                option = reader.readLine();
+                usersHolder.loging(reader);
+
+                if (option.equals("review")) {
+                    logHolder.allLogReader(usersHolder.getActualUser());
+                    if (usersHolder.getActualUser().getAccesLevel().equals("admin")) {
+                        System.out.println("Czy chciałbyś któryś Log usunąć? y/n");
+                        if (reader.readLine().equals("y")) {
+                            System.out.println("Wpisz ID loga który usuwasz:");
+                            logHolder.logRemover(reader.readLine());
+                        }
+                    }
+                } else if (option.equals("save")) {
+                    System.out.println("Enter the contents of the Log below: ");
+                    Log log = new Log(reader.readLine(), usersHolder.getActualUser());
+                    logHolder.addLog(log);
+                }
+
+
+                reader.close();
+            } catch (Exception e) {
+                e.getMessage();
+            }
+        }
+    }
+}
