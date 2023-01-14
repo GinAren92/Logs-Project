c07bef48a0a594cb81d80fa83dc6b049347c4739
Piotr Partyka <arenx92@gmail.com>
2023-01-14 19:22:23 +0100

    Reorganize old project to Maven

---
+++ src/main/java/org/example/Log.java
@@ -0,0 +1,41 @@
+package org.example;
+
+
+import java.time.LocalDateTime;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+import java.util.UUID;
+
+public class Log {
+    private static int allPostCount=0;
+    private String logId;
+    private LocalDateTime timestamp;
+    private String msg;
+    private User author;
+
+    public Log(String msg, User author){
+        this.msg=msg;
+        this.author=author;
+        timestamp = LocalDateTime.now();
+        logId= UUID.randomUUID().toString();
+        allPostCount++;
+    }
+
+    public String getLogId() {
+        return logId;
+    }
+
+    public LocalDateTime getTimestamp() {
+        return timestamp;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public User getAuthor() {
+        return author;
+    }
+}
+
