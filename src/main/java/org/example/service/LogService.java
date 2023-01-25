14016cd37d71bda0f8cb06f20cfb7de976f01484
Piotr Partyka <arenx92@gmial.com>
2023-01-25 11:30:36 +0100

    Write/Save rework

--- src/main/java/org/example/service/LogService.java
+++ src/main/java/org/example/service/LogService.java
@@ -27,17 +27,11 @@ public class LogService {
         Map<String, Log> listOfLogs = new HashMap<>();
         File file = new File("logs.json");
         if (file.exists()) {
-            AtomicBoolean attemptionReadingCondition = new AtomicBoolean(false);
-            int attemptionReadingLoopVal = 0;
-            do {
                 try {
                     tmpMap = OBJECT_MAPPER.readValue(new File("logs.json"), Map.class);
                 } catch (IOException e) {
-                    if(attemptionReadingLoopVal<=10) attemptionReadingCondition.set(true);
-                    else throw new Error(e.getCause());
+                    throw new Error(e);
                 }
-
-                int finalAttemptionReadingLoopVal = attemptionReadingLoopVal;
                 tmpMap.forEach((key, val) -> {
                     String jsonVal;
                     try {
@@ -45,33 +39,18 @@ public class LogService {
                         Log log = OBJECT_MAPPER.readValue(jsonVal, Log.class);
                         listOfLogs.put(key, log);
                     } catch (JsonProcessingException e) {
-                        if((finalAttemptionReadingLoopVal <= 10 && attemptionReadingCondition.get()) == false) attemptionReadingCondition.set(true);
-                        else throw new Error(e.getCause());
+                        throw new Error(e);
                     }
                 });
-                if(attemptionReadingLoopVal>10){
-                    attemptionReadingCondition.set(false);
-                    throw new Error("Exceeded 10 failed attempts to load the file.");
-                }else attemptionReadingLoopVal++;
-            }while(attemptionReadingCondition.get());
         }
             return listOfLogs;
     }
     public void writeToFile(Map<String,Log> listOfLogs){
-        AtomicBoolean attemptionWritingCondition = new AtomicBoolean(false);
-        int attemptionWritingLoopVal = 0;
-        do {
             try {
                 OBJECT_MAPPER.writerWithDefaultPrettyPrinter().writeValue(Paths.get("logs.json").toFile(), listOfLogs);
             } catch (IOException e) {
-                if(attemptionWritingLoopVal<=10) attemptionWritingCondition.set(true);
-                else throw new Error(e.getCause());
+                throw new Error(e);
             }
-            if(attemptionWritingLoopVal>10){
-                attemptionWritingCondition.set(false);
-                throw new Error("Exceeded 10 failed attempts to load the file.");
-            }else attemptionWritingLoopVal++;
-        }while(attemptionWritingCondition.get());
     }
 
     public void allLogReader(User actualUser, Map<String,Log> listOfLogs){
